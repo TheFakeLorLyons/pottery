9074918cf7ad04e491626bd2a1da336f7acc5e23
Lor <lorelailyons@protonmail.com>
2024-09-13 23:07:43 -0400

    Added some colors to the apples chart

--- src/pottery/clj/home.clj
+++ src/pottery/clj/home.clj
@@ -24,29 +24,47 @@
     first)
 
 (def apples
-  (tc/dataset "resources/assets/apples/apple_quality.csv" {:key-fn keyword}))
+  (tc/dataset "resources/assets/apples/apple_quality.csv"))
 
 `(-> apples
-    tc/select-columns [:Crunchiness :Ripeness])
+    tc/select-columns [:Crunchiness :Ripeness :Quality])
 
-(def get-apples
+(def color-mapping {"Good" "blue" "Bad" "red"})
+
+(def size-weight
   (kind/vega     ;fn call
-  (hc/xform ht/point-chart
-            :DATA (-> apples
-                      (tc/select-columns [:Crunchiness :Ripeness])
-                      (tc/rows :as-maps))
-            :X :Crunchiness :Y :Ripeness)));
+   (hc/xform ht/point-chart
+             :DATA (-> apples
+                       (tc/select-columns ["Size" "Weight" "Quality"])
+                       (tc/rows :as-maps))
+             :X :Size
+             :COLOR "Quality"
+             :Y :Weight)))
+;make a form here so I can drop down what I want to chart against 
+;what
+
+(def crunchiness-juiciness
+  (kind/vega     ;fn call
+   (hc/xform ht/point-chart
+             :DATA (-> apples
+                       (tc/select-columns ["Crunchiness" "Weight" "Juiciness"])
+                       (tc/rows :as-maps))
+             :X :Size
+             :COLOR "Quality"
+             :Y :Weight)))
 
 (def simple-data
   ^:kind/table
   {:column-names ["A" "B" "C"]
    :row-vectors (for [i (range 1 4)]
                   (for [j (range 1 4)]
-                    (* i j)))}); a map keeps the metadata because it evaluates to itself
+                    (* i j)))})
+; a map keeps the metadata because it evaluates to itself
 
 (kind/hiccup
  [:div
-  [:div get-apples]
+  [:div size-weight]
+  [:div crunchiness-juiciness]
   [:div simple-data]
   [:div table]
   [:div ^:kind/dataset (tc/head apples 10)]])
@@ -56,4 +74,3 @@
 (comment
   (clay/make! {:format [:html] 
                :source-path "src/pottery/clj/home.clj"}))
-
