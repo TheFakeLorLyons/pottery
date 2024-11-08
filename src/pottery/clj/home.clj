865b64b3f24fd834f35183624bc67ffc9368a9a5
Lor <lorelailyons@protonmail.com>
2024-11-08 01:15:36 -0500

    Fixed the layered charts and condensed the code.

--- src/pottery/clj/home.clj
+++ src/pottery/clj/home.clj
@@ -66,51 +66,37 @@
   (when (number? x)
     (and (>= x 0) (<= x 10))))
 
+(defn valid-trans-sample? [row]
+  (and #(valid-score? (get % "q01"))
+       (= (get row "trans_sample") 1)))
+
+(def get-superset
+  (memoize (fn [column pred]
+             (-> bulk-trans-data
+                 (tc/select-rows pred)
+                 (tc/group-by column)
+                 (tc/aggregate {"count" tc/row-count})
+                 (tc/rename-columns {column "score"})
+                 (tc/order-by [:score])
+                 (tc/rows :as-maps)))))
+
 (defn get-LGBT-stats [column]
-  (let [column-data
-        (-> bulk-trans-data
-        (tc/select-rows #(valid-score? (get % "q01")))
-        (tc/group-by column)
-        (tc/aggregate {"count" tc/row-count})
-        (tc/rename-columns {column "score"})
-        (tc/order-by [:score])
-        (tc/rows :as-maps))] 
-       column-data))
+  (get-superset column #(valid-score? (get % "q01"))))
 
 (defn get-trans-stats [column]
-  (-> bulk-trans-data
-      (tc/select-rows #(= (get % "trans_sample") 1))
-      (tc/select-rows #(valid-score? (get % "q01")))
-      (tc/group-by column)
-      (tc/aggregate {"count" tc/row-count})
-      (tc/rename-columns {column "score"})
-      (tc/order-by [:score])
-      (tc/rows :as-maps)))
+  (get-superset column valid-trans-sample?))
+
+(defn convert-to-percentage [processed-columns]
+  (let [total-count (reduce + (map #(get % "count") processed-columns))]
+    (map #(assoc % "percentage" (/ (get % "count") total-count)) processed-columns)))
 
 (defn get-LGBT-percentages [column]
-  (let [column-data
-        (-> bulk-trans-data
-            (tc/select-rows #(valid-score? (get % "q01")))
-            (tc/group-by column)
-            (tc/aggregate {"count" tc/row-count})
-            (tc/rename-columns {column "score"})
-            (tc/order-by [:score])
-            (tc/rows :as-maps))
-        total-count (reduce + (map #(get % "count") column-data))]
-    (map #(assoc % "percentage" (/ (get % "count") total-count)) column-data)))
+  (let [column-data (get-LGBT-stats column)]
+    (convert-to-percentage column-data)))
 
 (defn get-trans-percentages [column]
-  (let [column-data
-        (-> bulk-trans-data
-            (tc/select-rows #(= (get % "trans_sample") 1))
-            (tc/select-rows #(valid-score? (get % "q01")))
-            (tc/group-by column)
-            (tc/aggregate {"count" tc/row-count})
-            (tc/rename-columns {column "score"})
-            (tc/order-by [:score])
-            (tc/rows :as-maps))
-        total-count (reduce + (map #(get % "count") column-data))]
-    (map #(assoc % "percentage" (/ (get % "count") total-count)) column-data)))
+  (let [column-data (get-trans-stats column)]
+    (convert-to-percentage column-data)))
 
 (defn get-trans-stats-and-percentages [column]
   (let [trans-stats (get-trans-stats column)
@@ -158,15 +144,23 @@
   (let [combined-data (concat
                        (map #(assoc % "group" "LGBT Population") lgbt-items-mapped)
                        (map #(assoc % "group" "Trans Sample") trans-items-mapped))
-        total-count (reduce + (map #(get % "count") combined-data))
+        total-count (if is-percent
+                      (reduce + (map #(get % "count") lgbt-items-mapped))  ; Calculate separate totals
+                      1)
+        trans-total (if is-percent
+                      (reduce + (map #(get % "count") trans-items-mapped))
+                      1)
         data-to-use (if is-percent
-                      (map #(assoc % "count" (* 100 (/ (get % "count") total-count))) combined-data)
+                      (concat
+                       (map #(assoc % "count" (* 100 (/ (get % "count") total-count)))
+                            (filter #(= (get % "group") "LGBT Population") combined-data))
+                       (map #(assoc % "count" (* 100 (/ (get % "count") trans-total)))
+                            (filter #(= (get % "group") "Trans Sample") combined-data)))
                       combined-data)]
     (kind/vega-lite
      {:data {:values data-to-use}
       :width 600
       :height 400
-      :mark {:type "bar" :opacity 0.7}
       :encoding {:x {:field "response"
                      :type "ordinal"
                      :axis {:title (str column-name " Responses")
@@ -188,14 +182,13 @@
                               :type "quantitative"}
                           :color {:field "group"
                                   :type "nominal"
-                                  :scale {:range ["lightblue" "pink"]}}}}
-              {:mark {:type "text" :align "center" :baseline "middle" :dy -5}
-               :encoding {:x {:field "response"}
-                          :y {:field "count"}
-                          :text {:field "count"}}}]})))
-
-
-
+                                  :scale {:range ["lightblue" "pink"]}}
+                          :text {:field "count"
+                                 :format ".1f"}}}
+              {:mark {:type "text"
+                      :align "center"
+                      :baseline "middle"
+                      :dy -5}}]})))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -223,14 +216,14 @@
 
         lgbt-chart (individual-chart lgbt-items-mapped "lightblue" (str column-name " - LGBT Population") responses lgbt-items-mapped false)
         trans-chart (individual-chart trans-items-mapped "pink" (str column-name " - Transgender Sample") responses trans-items-mapped false)
-        layered-chart (layered-chart lgbt-items-mapped trans-items-mapped responses (str column-name " - Compared") false)
+        layered-stats-chart (layered-chart lgbt-items-mapped trans-items-mapped responses (str column-name " - Compared") false)
         lgbt-per-chart (individual-chart lgbt-percent-mapped "lightblue" (str column-name " - LGBT Population (%)") responses lgbt-percent-mapped true)
         trans-per-chart (individual-chart trans-percent-mapped "pink" (str column-name " - Transgender Sample (%)") responses trans-percent-mapped true)
         percent-layered (layered-chart lgbt-percent-mapped trans-percent-mapped responses (str column-name " - Compared (%)") true)]
     {:subheading (str column-name " - " (get question-info :label))
      :lgbt-chart lgbt-chart
      :trans-chart trans-chart
-     :layered-chart layered-chart
+     :layered-stats-chart layered-stats-chart
      :lgbt-percent-chart lgbt-per-chart
      :trans-percent-chart trans-per-chart
      :percent-layered percent-layered}))
@@ -251,7 +244,7 @@
       [:h5 (:subheading graphs)]
       [:div (:lgbt-chart graphs)
        (:trans-chart graphs)]
-      [:div (:layered-chart graphs)]
+      [:div (:layered-stats-chart graphs)]
       [:div (:lgbt-percent-chart graphs)
        (:trans-percent-chart graphs)]
       [:div (:percent-layered graphs)]])])
@@ -266,7 +259,7 @@
                 :text-align "center"
                 :margin-bottom "5vh"
                 :gap "10vh"}}
-  
+
   [:h2 "Practice Set 1 - Apples"
    [:div size-weight]
    [:div create-3d-scatter-plot]]
@@ -276,5 +269,5 @@
    [:div "Thanks for checking this out. More of my work can be seen on my website - lorelailyons.me"]]])
 
 (comment
-  (clay/make! {:format [:html] 
+  (clay/make! {:format [:html]
                :source-path "src/pottery/clj/home.clj"}))
